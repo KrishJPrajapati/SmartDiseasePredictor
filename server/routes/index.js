b92a59ece949d5c5572e3b80e25de8de2ce482fc
Nishu <nishupatel25112003@gmail.com>
2025-03-21 23:37:03 +0530

    Defined all routes in router object, now all routes are in format of api/{route}

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const register = require('../controller/register')
+const updateUserDetails = require('../controller/updateUserDetails')
+const userDetails = require('../controller/userDetails')
+const checkPassword = require('../controller/checkPassword')
+const checkEmail = require('../controller/checkEmail')
+
+router.post('/register', register)
+
+router.post('/email', checkEmail)
+
+router.post('/password', checkPassword)
+
+router.get('/user-details', userDetails)
+
+router.post('update-user', updateUserDetails)
 
 module.exports = router
